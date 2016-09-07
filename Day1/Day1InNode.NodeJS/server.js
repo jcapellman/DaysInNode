5644f432d729585fd15c7dca01bdb1c5a21a45a6
Jarred Capellman <jcapellman@hotmail.com>
2016-09-06 21:21:35 -0400

    Initial Commit of Day 1 Benchmarks

---
+++ Day1/Day1InNode.NodeJS/server.js
@@ -0,0 +1,10 @@
+﻿var express = require('express');
+
+var app = express();
+
+app.get('/api/Test',
+    function(req, res) {
+        res.json({ message: new Date().toLocaleString() });
+    });
+
+app.listen(1338);
