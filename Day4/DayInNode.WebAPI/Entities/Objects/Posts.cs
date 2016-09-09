4433d2047e82c7ece50d3ef4063855617ba54994
Jarred Capellman <jcapellman@hotmail.com>
2016-09-09 14:24:43 -0400

    Added Day4 Blog Post related code

---
+++ Day4/DayInNode.WebAPI/Entities/Objects/Posts.cs
@@ -0,0 +1,12 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace DayInNode.WebAPI.Entities.Objects {
+    [Table("POSTS")]
+    public class Posts {
+        [Key]
+        public int ID { get; set; }
+        
+        public int Likes { get; set; }
+    }
+}
