29c52c91935c925db5e746905aa0762cf3302f49
Jarred Capellman <jcapellman@hotmail.com>
2016-09-10 16:24:12 -0700

    Added Mongoose references for Day5 deep dive

--- Day5/DayInNode.NodeJS/server.js
+++ Day5/DayInNode.NodeJS/server.js
@@ -4,25 +4,14 @@ var collections = ['posts'];
 var express = require('express');
 var mongojs = require('mongojs');
 
+var testHttp = require('TestHandler');
+
 var app = express();
 
 var db = mongojs(dburl, collections);
 
 var postData = db.collection('posts');
 
-app.get('/api/Test',
-    function (request, response) {
-        var id = request.params.id;
-
-        var newData = { 'id': id, 'likes': 2 };
-        
-        postData.insert(newData, function(err, post) {
-            if (err) {
-                return response.json({ message: err });
-            }
-            
-            return response.json({ message: true });
-        });        
-    });
+app.get('/api/Test', testHttp.get(request, response));
 
 app.listen(1338);
