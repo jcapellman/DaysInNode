2b3eb22195f6a7407f84e76407fd6252c908f5dc
Jarred Capellman <jcapellman@hotmail.com>
2016-09-10 19:43:03 -0700

    Added routes.js and revised server.js for Day5

---
+++ Day5/DayInNode.NodeJS/routes.js
@@ -0,0 +1,28 @@
+﻿var express = require('express');
+var mongojs = require('mongojs');
+
+var dburl = 'localhost/day2innode';
+var collections = ['posts'];
+
+var db = mongojs(dburl, collections);
+
+var postData = db.collection('posts');
+
+var router = express.Router();
+
+router.get('/api/Test', function (request, response) {
+    var id = request.params.id;
+
+    var newData = { 'id': id, 'likes': 2 };
+
+    postData.insert(newData, function (err, post) {
+        if (err) {
+            return response.json({ message: err });
+        }
+
+        return response.json({ message: true });
+    });
+});
+
+
+module.exports = router;
--- Day5/DayInNode.NodeJS/server.js
+++ Day5/DayInNode.NodeJS/server.js
@@ -1,17 +1,7 @@
-﻿var dburl = 'localhost/day2innode';
-var collections = ['posts'];
-
-var express = require('express');
-var mongojs = require('mongojs');
-
-var testHttp = require('TestHandler');
+﻿var express = require('express');
 
 var app = express();
 
-var db = mongojs(dburl, collections);
-
-var postData = db.collection('posts');
-
-app.get('/api/Test', testHttp.get(request, response));
+app.use(require('./routes'));
 
 app.listen(1338);
