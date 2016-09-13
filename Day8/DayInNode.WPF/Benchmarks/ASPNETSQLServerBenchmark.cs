4af754848b6059645880a539c17e0523dd4ad5aa
Jarred Capellman <jcapellman@hotmail.com>
2016-09-12 21:05:07 -0700

    Added pm2 reference in the Day8 code

---
+++ Day8/DayInNode.WPF/Benchmarks/ASPNETSQLServerBenchmark.cs
@@ -0,0 +1,7 @@
+﻿namespace DayInNode.WPF.Benchmarks {
+    public class ASPNETSQLServerBenchmark : BaseBenchmark {
+        public override string GetName() => "ASP.NET WebAPI with SQL Server";
+
+        public override int GetPort() => 1337;
+    }
+}
---
+++ Day8/DayInNode.WebAPI/Controllers/TestMongoController.cs
@@ -0,0 +1,28 @@
+﻿using System.Threading.Tasks;
+
+using Microsoft.AspNetCore.Mvc;
+
+using MongoDB.Bson;
+using MongoDB.Driver;
+
+namespace DayInNode.WebAPI.Controllers {
+    [Route("api/[controller]")]
+    public class TestMongoController : Controller {
+        [HttpGet]
+        public async Task<string> Get(int id) {
+            var client = new MongoClient("mongodb://localhost");
+            var database = client.GetDatabase("day2innode");
+
+            var collection = database.GetCollection<BsonDocument>("posts");
+
+            var document = new BsonDocument {
+                { "id", id },
+                { "likes", 2 }
+            };
+
+            await collection.InsertOneAsync(document);
+
+            return "OK";
+        }
+    }
+}
