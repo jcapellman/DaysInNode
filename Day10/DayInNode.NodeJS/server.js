64d2e8e36bd63c72c7951f27156200286be78a52
Jarred Capellman <jcapellman@hotmail.com>
2016-09-14 22:12:34 -0400

    Added Day Ten Code focusing on Redis

---
+++ Day10/DayInNode.NodeJS/server.js
@@ -0,0 +1,11 @@
+﻿var restify = require('restify');
+var settings = require('./config');
+var testRouter = require('./test.router'); 
+
+var server = restify.createServer();
+
+server.use(restify.queryParser());
+
+testRouter.applyRoutes(server);
+
+server.listen(settings.HTTP_SERVER_PORT);
---
+++ Day10/DayInNode.WPF/Benchmarks/ASPNETMongoDBBenchmark.cs
@@ -0,0 +1,9 @@
+﻿namespace DayInNode.WPF.Benchmarks {
+    public class ASPNETMongoDBBenchmark : BaseBenchmark {
+        public override string GetName() => "ASP.NET WebAPI with MongoDB";
+
+        public override int GetPort() => 1337;
+
+        public override string MethodName() => "TestMongo";
+    }
+}
